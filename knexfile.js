32619a94321676865a7c76498b8414e92084e55f
RenanTCastro <renancastrotut@gmail.com>
2025-08-06 16:54:12 -0300

    change: add pool

--- knexfile.js
+++ knexfile.js
@@ -23,6 +23,10 @@ module.exports = {
       host: '127.0.0.1',
       port: 3306
     },
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       tableName: "knex_migrations",
       directory: `${__dirname}/src/database/migrations`,
