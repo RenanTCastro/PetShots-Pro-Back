8f26cee6706eaf0b2cdb0f928595599ba0b40d4a
RenanTCastro <renancastrotut@gmail.com>
2025-08-06 14:13:01 -0300

    change: add config production

--- knexfile.js
+++ knexfile.js
@@ -13,4 +13,20 @@ module.exports = {
       directory: `${__dirname}/src/database/migrations`,
     },
   },
+
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT || 3306,
+      ssl: { rejectUnauthorized: true },
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
+    },
+  },
 };
